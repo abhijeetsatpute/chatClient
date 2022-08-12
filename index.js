3ed904eb1419a9cfe68b7e4cd1dbbc7d38dc15d6
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-12 06:35:36 +0530

    POST messages
    + body parser for JSON values with POST
    + body parser for url encoded values over browser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const app = express();
 
 app.use(express.static(__dirname))
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
 
 const messages = [
     {name:"Abhijeet",message:"Hello, Abhijeet!"},
@@ -12,6 +15,11 @@ const messages = [
 
 app.get('/messages', (req, res) => {
     res.send(messages);
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    res.sendStatus(200);
 })
 
 const server = app.listen(3000, () => {
