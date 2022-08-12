39dec39d2c3a2008b170db8dabf496b76aa6a5ce
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-12 06:17:20 +0530

    GET messages

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.use(express.static(__dirname))
 
+const messages = [
+    {name:"Abhijeet",message:"Hello, Abhijeet!"},
+    {name:"Foo",message:"Hello, Foo!"},
+    {name:"Poo",message:"Hello, Poo!"}
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+})
+
 const server = app.listen(3000, () => {
     console.log(`Server running at http://localhost:${server.address().port}`);
 });
